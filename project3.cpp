d29eb3fe40a15cfc8634368b3d3248030aacb11b
osboxes.org <jfgerulskis@wpi.edu>
2019-12-06 14:17:49 -0500

    blah

--- project3.cpp
+++ project3.cpp
@@ -9,14 +9,13 @@
 
 // router 
 void startRouter(char *param);
+void fowardData(int routerFD);
 
 // host
 void startHost(char *param);
-void sendDataToRouter(char *routerIP, char *hostIP, char *TTL);
 bool isDataToSend();
-void receiveDataFromHost(char *data, char *hostIP, char *TTL);
-void sendDataToHost(char* vmIP, char* data);
-void receiveDataFromRouter();
+void sendData(struct sockaddr_in routerAddr, int socketFD);
+void recvData(struct sockaddr_in routerAddr, int socketFD);
 
 
 int main(int argc, char *argv[]) {
@@ -37,23 +36,12 @@ int main(int argc, char *argv[]) {
     } else {
         startHost(argv[2]);
     }
-
-    // const char delimeter[2] = ",";
-    // char *token;
-    
-    // /* get the first token */
-    // token = strtok(argv[2], delimeter);
-    
-    // /* walk through other tokens */
-    // while( token != NULL ) {
-    //     printf( "%s\n", token );
-    //     token = strtok(NULL, delimeter);
-    // }
 }
 
 // Router code below here
 
 void startRouter(char *param) {
+    // 1. Parse params
 	char *token = strtok(param, ",:");
 	std::map<std::string, char *> table; 
 	while (token !=NULL){
@@ -81,15 +69,44 @@ void startRouter(char *param) {
     char hostIP[100];
     char TTL[100];
 
-    receiveDataFromHost(data, hostIP, TTL);
-
     std::string ip(hostIP);
     std::cout << "Overlay IP: " << ip << std::endl;
 
     char* hostvmIP = table.find(ip)->second;
     std::cout << "VM IP: " << hostvmIP << std::endl;
 
-    sendDataToHost(hostvmIP, data);
+    // 2. Set up and bind server
+    struct sockaddr_in servaddr; 
+    bzero(&servaddr, sizeof(servaddr)); 
+  
+    int routerFD = socket(AF_INET, SOCK_DGRAM, 0);         
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+    servaddr.sin_port = htons(2012); 
+    servaddr.sin_family = AF_INET;  
+   
+    // 3. bind server address to socket descriptor 
+    bind(routerFD, (struct sockaddr*)&servaddr, sizeof(servaddr));
+
+    // 4. foward data
+    fowardData(routerFD);
+}
+
+void fowardData(int routerFD) {
+
+    //receive the datagram 
+    char buffer[100];
+    int n = recv(routerFD, buffer, sizeof(buffer), 0);
+    buffer[n] = '\0'; 
+    printf("Host Sent: %s", buffer);
+
+    //build client addr
+    char *message = "hello host \n\nsincerely, \nthe router";
+    struct sockaddr_in cliaddr;
+    cliaddr.sin_addr.s_addr = inet_addr(buffer);  // TODO: make client ip
+    cliaddr.sin_port = htons(2012); 
+    cliaddr.sin_family = AF_INET;  
+    // send data to client
+    sendto(routerFD, message, 1000, 0, (struct sockaddr*)&cliaddr, sizeof(cliaddr)); 
 }
 
 // =======================
@@ -97,9 +114,8 @@ void startRouter(char *param) {
 
 void startHost(char *param) {
     const char delimeter[2] = ",";
-    //char *token;
     
-    /* routerIP,hostIP,TTL */
+    // 1. Parse parameters
     char* routerIP = strtok(param, delimeter);
     char* hostIP = strtok(NULL, delimeter);
     char* timeToLive = strtok(NULL, delimeter);
@@ -109,8 +125,19 @@ void startHost(char *param) {
     }
     printf("Starting host with parameters router IP: %s, host IP: %s, TTL:, %s\n", routerIP, hostIP, timeToLive);
     
-    sendDataToRouter(routerIP, hostIP, timeToLive);
-    receiveDataFromRouter();
+    // 2. Set up server credentials
+    struct sockaddr_in servaddr;  
+    bzero(&servaddr, sizeof(servaddr)); 
+    servaddr.sin_addr.s_addr = inet_addr(routerIP);
+    servaddr.sin_port = htons(2012); 
+    servaddr.sin_family = AF_INET; 
+
+    // 3. Set up host socket 
+    int hostSocket = socket(AF_INET, SOCK_DGRAM, 0); 
+
+    // 4. Handle data
+    sendData(servaddr, hostSocket);
+    recvData(servaddr, hostSocket);
 }
 
 /**
@@ -121,152 +148,16 @@ bool isDataToSend() {
     return access("tosend.bin", R_OK) != -1;
 }
 
-/**
- * @param routerIP - The IP of the router
- */
-void sendDataToRouter(char* routerIP, char* hostIP, char* TTL) {
-
-    char buffer[100]; 
-    char *message = "Data File"; 
-    int sockfd; 
-    struct sockaddr_in servaddr; 
-      
-    // clear servaddr 
-    bzero(&servaddr, sizeof(servaddr)); 
-    servaddr.sin_addr.s_addr = inet_addr(routerIP);
-    servaddr.sin_port = htons(2012); 
-    servaddr.sin_family = AF_INET; 
-      
-    // create datagram socket 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
-      
-    // connect to server 
-    if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) 
-    { 
-        printf("\n Error : Connect Failed \n"); 
-        exit(0); 
-    } 
-  
-    // request to send datagram 
-    // no need to specify server address in sendto 
-    // connect stores the peers IP and port 
-    sendto(sockfd, message, 1000, 0, (struct sockaddr*)NULL, sizeof(servaddr)); 
-    sendto(sockfd, hostIP, 1000, 0, (struct sockaddr*)NULL, sizeof(servaddr)); 
-    sendto(sockfd, TTL, 1000, 0, (struct sockaddr*)NULL, sizeof(servaddr)); 
-      
-    // waiting for response 
-    recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL); 
-    puts(buffer); 
-  
-    // close the descriptor 
-    close(sockfd); 
-
+void sendData(struct sockaddr_in routerAddr, int socketFD) {
+    char *message = "10.0.2.15"; 
+    socklen_t len = sizeof(routerAddr);
+    sendto(socketFD, message, 1000, 0, (struct sockaddr*)&routerAddr, sizeof(routerAddr)); 
 }
 
-void receiveDataFromHost(char *data, char *hostIP, char *TTL){
-
-    char *message = "Data Sent to Router"; 
-    int listenfd;
-    socklen_t len; 
-    struct sockaddr_in servaddr, cliaddr; 
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // Create a UDP Socket 
-    listenfd = socket(AF_INET, SOCK_DGRAM, 0);         
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(2012); 
-    servaddr.sin_family = AF_INET;  
-   
-    // bind server address to socket descriptor 
-    bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)); 
-       
-    //receive the datagram 
-    len = sizeof(cliaddr); 
-    int n = recvfrom(listenfd, data, 100, 
-            0, (struct sockaddr*)&cliaddr,&len); //receive message from server 
-    data[n] = '\0';     
-
-    int m = recvfrom(listenfd, hostIP, 100, 
-            0, (struct sockaddr*)&cliaddr,&len); //receive message from server
-    hostIP[m] = '\0';
-
-    int o = recvfrom(listenfd, TTL, 100, 
-            0, (struct sockaddr*)&cliaddr,&len); //receive message from server 
-    TTL[o] = '\0';
-           
-    // send the response 
-    sendto(listenfd, message, 1000, 0, 
-        (struct sockaddr*)&cliaddr, sizeof(cliaddr)); 
-}
-
-void sendDataToHost(char* vmIP, char* data) {
-
+void recvData(struct sockaddr_in routerAddr, int socketFD) {
     char buffer[100];
-    char *message = "Data Received:"; 
-    int sockfd; 
-    struct sockaddr_in servaddr; 
-      
-    // clear servaddr 
-    bzero(&servaddr, sizeof(servaddr)); 
-    servaddr.sin_addr.s_addr = inet_addr(vmIP);
-    servaddr.sin_port = htons(2012); 
-    servaddr.sin_family = AF_INET; 
-      
-    // create datagram socket 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
-      
-    // connect to server 
-    if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) 
-    { 
-        printf("\n Error : Connect Failed \n"); 
-        exit(0); 
-    } 
-  
-    // request to send datagram 
-    // no need to specify server address in sendto 
-    // connect stores the peers IP and port 
-    sendto(sockfd, message, 1000, 0, (struct sockaddr*)NULL, sizeof(servaddr));
-    sendto(sockfd, data, 1000, 0, (struct sockaddr*)NULL, sizeof(servaddr));  
-      
-    // waiting for response 
-    recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL); 
-    puts(buffer); 
-  
-    // close the descriptor 
-    close(sockfd);
-}
-
-void receiveDataFromRouter(){
-    char buf[100];
-    char buf2[100];
-    char *message = "Data Sent to Receiver Host"; 
-    int listenfd;
-    socklen_t len; 
-    struct sockaddr_in servaddr, cliaddr; 
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // Create a UDP Socket 
-    listenfd = socket(AF_INET, SOCK_DGRAM, 0);         
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(2012); 
-    servaddr.sin_family = AF_INET;  
-   
-    // bind server address to socket descriptor 
-    bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)); 
-       
-    //receive the datagram 
-    len = sizeof(cliaddr); 
-    int n = recvfrom(listenfd, buf, sizeof(buf), 
-            0, (struct sockaddr*)&cliaddr,&len); //receive message from server 
-    buf[n] = '\0';
-    puts(buf);
-
-    int m = recvfrom(listenfd, buf2, sizeof(buf2), 
-            0, (struct sockaddr*)&cliaddr,&len); //receive message from server 
-    buf2[m] = '\0';
-    puts(buf2);    
-           
-    // send the response 
-    sendto(listenfd, message, 1000, 0, 
-        (struct sockaddr*)&cliaddr, sizeof(cliaddr)); 
+    socklen_t len = sizeof(routerAddr);
+    int n = recvfrom(socketFD, buffer, sizeof(buffer), 0, (struct sockaddr*)&routerAddr, &len);
+    buffer[n] = '\0'; 
+    printf("Response: %s", buffer);
 }
